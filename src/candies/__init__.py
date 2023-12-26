55f4a280271cf323906ce34a07ebcae0f416ccea
Ujjwal Panda <ujjwalpanda97@gmail.com>
2023-12-26 17:05:50 +0530

    🎨 Large structural changes.
    This commit makes the code easier to use, by moving things back from
    separate modules back into the `core` module. This was done for both
    processing and plotting the candidates. This commit also introduces a
    feature where candidates can be loaded in from HDF5 files.

--- src/candies/__init__.py
+++ src/candies/__init__.py
@@ -3,14 +3,5 @@ Sweet, sweet candy-dates!
 """
 
 from candies.core import Candy, Candies
-from candies.plotting import plot_candy, plot_candies
-from candies.processing import process_candy, process_candies
 
-__all__ = [
-    "Candy",
-    "Candies",
-    "plot_candy",
-    "plot_candies",
-    "process_candy",
-    "process_candies",
-]
+__all__ = ["Candy", "Candies"]
--- src/candies/core.py
+++ src/candies/core.py
@@ -3,13 +3,20 @@ Generally applicable code for Candies.
 """
 
 import mmap
+import warnings
 import h5py as h5
 import numpy as np
+import proplot as pplt
+from numba import cuda
+from typing import Any
 from attrs import define
 from pathlib import Path
 from priwo import readhdr
+from rich.progress import track
 from typing_extensions import Self
+from candies.utilities import dmt_extent
 from collections.abc import MutableSequence
+from candies.kernels import fastdmt, dedisperse
 
 from candies.utilities import (
     read_csv,
@@ -17,6 +24,9 @@ from candies.utilities import (
     dispersive_delay,
     read_astroaccelerate,
 )
+
+
+warnings.filterwarnings("ignore")
 
 
 class CandiesError(Exception):
@@ -50,8 +60,8 @@ class Candy:
     tobs: float
 
     # Hidden internal parameters.
-    skip: int
-    mm: mmap.mmap
+    skip: int | None = None
+    mm: mmap.mmap | None = None
 
     # Optional parameters.
     ndms: int | None = None
@@ -70,13 +80,13 @@ class Candy:
         bw: float,
         fh: float,
         fl: float,
-        skip: int,
         t0: float,
         dm: float,
         snr: float,
         tobs: float,
-        mm: mmap.mmap,
         wbin: int | float,
+        skip: int | None = None,
+        mm: mmap.mmap | None = None,
     ) -> Self:
         """
         Create and wrap up a candy-date.
@@ -98,6 +108,47 @@ class Candy:
             wbin=int(wbin),
         )
 
+    @classmethod
+    def load(cls, fn: str | Path) -> Self:
+        """
+        Load a candy-date as an HDF5 dataset.
+        """
+        with h5.File(fn, "r") as f:
+            t0 = f.attrs["t0"]
+            nf = f.attrs["nf"]
+            fh = f.attrs["fh"]
+            fl = f.attrs["fl"]
+            df = f.attrs["df"]
+            nt = f.attrs["nt"]
+            dt = f.attrs["dt"]
+            dm = f.attrs["dm"]
+            snr = f.attrs["snr"]
+            wbin = f.attrs["wbin"]
+            ndms = f.attrs["ndms"]
+            dmlow = f.attrs["dmlow"]
+            dmhigh = f.attrs["dmhigh"]
+            dmt = np.asarray(f["data_dm_time"])
+            dyn = np.asarray(f["data_freq_time"])
+        return cls(
+            fl=fl,
+            fh=fh,
+            df=df,
+            dt=dt,
+            nt=nt,
+            nf=nf,
+            t0=t0,
+            dm=dm,
+            snr=snr,
+            dmt=dmt,
+            dyn=dyn,
+            wbin=wbin,
+            ndms=ndms,
+            dmlow=dmlow,
+            dmhigh=dmhigh,
+            bw=df * nf,  # type: ignore
+            tobs=nt * dt,  # type: ignore
+        )
+
     @property
     def id(self) -> str:
         """
@@ -115,27 +166,126 @@ class Candy:
         start or end of the file, and enough data is not available, this
         function will pad the array with median values.
         """
-        maxdd = dispersive_delay(self.fl, self.fh, self.dm)
-        ti = self.t0 - maxdd - (self.wbin * self.dt)
-        tf = self.t0 + maxdd + (self.wbin * self.dt)
-        return np.pad(
-            np.frombuffer(
-                self.mm,
-                dtype=np.uint8,
-                count=int((tf - (0.0 if ti < 0.0 else ti)) / self.dt) * self.nf,
-                offset=int(0.0 if ti < 0.0 else ti / self.dt) * self.nf + self.skip,
-            )
-            .reshape(-1, self.nf)
-            .T,
-            (
-                (0, 0),
+        if (self.mm is not None) and (self.skip is not None):
+            maxdd = dispersive_delay(self.fl, self.fh, self.dm)
+            ti = self.t0 - maxdd - (self.wbin * self.dt)
+            tf = self.t0 + maxdd + (self.wbin * self.dt)
+            return np.pad(
+                np.frombuffer(
+                    self.mm,
+                    dtype=np.uint8,
+                    count=int((tf - (0.0 if ti < 0.0 else ti)) / self.dt) * self.nf,
+                    offset=int(0.0 if ti < 0.0 else ti / self.dt) * self.nf + self.skip,
+                )
+                .reshape(-1, self.nf)
+                .T,
                 (
-                    int(-ti / self.dt) if ti < 0.0 else 0,
-                    int((tf - self.tobs) / self.dt) if tf > self.tobs else 0,
+                    (0, 0),
+                    (
+                        int(-ti / self.dt) if ti < 0.0 else 0,
+                        int((tf - self.tobs) / self.dt) if tf > self.tobs else 0,
+                    ),
                 ),
-            ),
-            mode="median",
+                mode="median",
+            )
+        else:
+            raise CandiesError("Data has not yet been read from file.")
+
+    def process(
+        self,
+        ndms: int = 256,
+        save: bool = True,
+        zoom: bool = True,
+        stream: Any | None = None,
+    ):
+        """
+        Process a single candy-date.
+        """
+        cuda_params: dict = {}
+        if stream is None:
+            stream = cuda.stream()
+            cuda_params = {"stream": stream}
+
+        data = self.chop()
+        _, self.nt = data.shape
+
+        if zoom:
+            _, _, dmlow, dmhigh = dmt_extent(
+                self.fl,
+                self.fh,
+                self.dt,
+                self.t0,
+                self.dm,
+                self.wbin,
+            )
+        else:
+            dmlow, dmhigh = 0.0, 2.0 * self.dm
+
+        self.ndms = ndms
+        self.dmlow = dmlow
+        self.dmhigh = dmhigh
+        ddm = (dmhigh - dmlow) / (ndms - 1)
+
+        tfactor = 1 if np.log2(self.wbin) < 3 else np.log2(self.wbin) // 2
+        ffactor = np.floor(self.nf // 256).astype(int)
+        nfdown = int(self.nf / ffactor)
+        ntdown = int(self.nt / tfactor)
+
+        ft = cuda.to_device(data, **cuda_params)
+        dmtx = cuda.device_array((ndms, ntdown), order="C", **cuda_params)
+        dynx = cuda.to_device(np.zeros((nfdown, ntdown), order="C"), **cuda_params)
+
+        dedisperse[  # type: ignore
+            (self.nf // 32, self.nt // 32),
+            (32, 32),
+            stream,
+        ](
+            dynx,
+            ft,
+            self.nf,
+            self.nt,
+            self.df,
+            self.dt,
+            self.fh,
+            self.dm,
+            ffactor,
+            tfactor,
         )
+
+        fastdmt[  # type: ignore
+            self.nt,
+            self.ndms,
+            stream,
+        ](
+            dmtx,
+            ft,
+            self.nf,
+            self.nt,
+            self.df,
+            self.dt,
+            self.fh,
+            ddm,
+            dmlow,
+            tfactor,
+        )
+
+        dyn = dynx.copy_to_host(**cuda_params)
+        dmt = dmtx.copy_to_host(**cuda_params)
+
+        ntmid = int(ntdown / 2)
+        nticrop = ntmid - 128
+        ntfcrop = ntmid + 128
+        dmt = dmt[:, nticrop:ntfcrop]
+        dyn = dyn[:, nticrop:ntfcrop]
+
+        self.dyn = dyn
+        self.dmt = dmt
+
+        if save:
+            self.save()
+
+        if stream is None:
+            cuda.close()
 
     def save(self):
         """
@@ -184,6 +334,67 @@ class Candy:
                 )
                 dmtset.dims[0].label = b"dm"
                 dmtset.dims[1].label = b"time"
+
+    def plot(
+        self,
+        save: bool = True,
+        show: bool = False,
+    ):
+        """
+        Plot a candy-date.
+        """
+        if (self.dyn is not None) and (self.dmt is not None):
+            pplt.rc.update(
+                {
+                    "font.size": 20,
+                    "suptitle.pad": 20,
+                    "subplots.tight": True,
+                    "subplots.refwidth": 6.5,
+                    "subplots.panelpad": 1.5,
+                }
+            )
+
+            ntmid = int(self.nt / 2)  # type: ignore
+            nti = ntmid - 128
+            ntf = ntmid + 128
+            ts = self.dyn.sum(axis=0)
+            f = np.linspace(self.fh, self.fl, self.dyn.shape[0])  # type: ignore
+            dms = np.linspace(self.dmhigh, self.dmlow, self.ndms)  # type: ignore
+            t = np.linspace(nti * self.dt, ntf * self.dt, self.dyn.shape[1])  # type: ignore
+
+            fig, axs = pplt.subplots(
+                nrows=2,
+                ncols=1,
+                width=10,
+                height=15,
+                sharey=False,
+                sharex="labels",
+            )
+            px = axs[0].panel_axes("t", width="5em")
+
+            px.plot(t, ts)
+            axs[0].pcolormesh(*np.meshgrid(t, f), self.dyn)
+            axs[1].pcolormesh(*np.meshgrid(t, dms), self.dmt)
+
+            px.format(xlabel="Time (in s)", ylabel="Flux (arb. units)")
+            axs[0].format(
+                xlabel="Time (in s)",
+                ylabel="Frequency (in MHz)",
+                title="Dedispersed dynamic spectrum",
+            )
+
+            axs[1].format(
+                xlabel="Time (in s)",
+                ylabel="DM (in pc cm$^-3$)",
+                title=f"DMT, from {self.dmlow:.2f} to {self.dmhigh:.2f} pc cm$^-3$",
+            )
+
+            if save:
+                fig.savefig(f"{id}.png", dpi=300)
+            if show:
+                pplt.show()
+        else:
+            raise CandiesError("Candy-date has nto yet been processed.")
 
 
 @define
@@ -261,3 +472,49 @@ class Candies(MutableSequence):
                 for value in values
             ]
         )
+
+    @classmethod
+    def load(cls, fns: list[str | Path]) -> Self:
+        """
+        Load candy-dates from HDF5 datasets.
+        """
+        return cls(items=[Candy.load(fn) for fn in fns])
+
+    def process(
+        self,
+        ndms: int = 256,
+        device: int = 0,
+        save: bool = True,
+        zoom: bool = True,
+    ):
+        """
+        Process candy-dates.
+        """
+        cuda.select_device(device)
+        stream = cuda.stream()
+        for item in track(self.items, description="Processing candy-dates..."):
+            item.process(
+                ndms,
+                save,
+                zoom,
+                stream,
+            )
+        cuda.close()
+
+    def save(self):
+        """
+        Save candy-dates as HDF5 datasets.
+        """
+        for item in track(self.items, description="Saving candy-dates..."):
+            item.save()
+
+    def plot(
+        self,
+        save: bool = True,
+        show: bool = False,
+    ):
+        """
+        Plot candy-dates.
+        """
+        for item in track(self.items, description="Plotting candy-dates..."):
+            item.plot(save=save, show=show)
